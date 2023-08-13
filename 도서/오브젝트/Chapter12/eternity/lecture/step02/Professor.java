23a645f5d03fdfdd60ca16f410441d6a08ec5670
anwjrrp33 <anwjrrp33@gmail.com>
2023-08-13 15:13:05 +0900

    오브젝트

---
+++ 도서/오브젝트/Chapter12/eternity/lecture/step02/Professor.java
@@ -0,0 +1,16 @@
+package 도서.오브젝트.Chapter12.eternity.lecture.step02;
+
+public class Professor {
+    private String name;
+    private Lecture lecture;
+
+    public Professor(String name, Lecture lecture) {
+        this.name = name;
+        this.lecture = lecture;
+    }
+
+    public String compileStatistics() {
+        return String.format("[%s] %s - Avg: %.1f", name,
+                lecture.evaluate(), lecture.average());
+    }
+}
